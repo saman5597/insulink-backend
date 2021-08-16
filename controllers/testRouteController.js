7cf11870ded79b64b17b903a2ac540a8994ce454
Saman Arshad <saman@agvahealthcare.com>
2021-08-16 11:22:16 +0530

    E: Added new test route

--- controllers/testRouteController.js
+++ controllers/testRouteController.js
@@ -33,6 +33,37 @@ exports.getAllUsersNew = async (req, res) => {
     }
 }
 
+exports.getUserByUserIdNew = async (req, res) => {
+    try {
+        var queryObj
+        if (req.query["status"] === "inactive") {
+            queryObj = { _id: req.params.id, status: "inactive" }
+        } else if (req.query["status"] === "active") {
+            queryObj = { _id: req.params.id, status: "active" }
+        } else queryObj = { _id: req.params.id }
+
+        const user = await User.find(queryObj).populate({
+            path: "devices",
+            select: "_id serialNo modelType manufactureDate battery reservoir"
+        })
+        // .select('-devices')
+
+        res.status(200).json({ status: 1, data: { user }, message: 'Getting data of user by user id from DB.' })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            status: -1,
+            data: {
+                err: {
+                    generatedTime: new Date(),
+                    errMsg: err.message,
+                    msg: 'Internal Server Error.',
+                    type: err.name
+                }
+            }
+        })
+    }
+}
 
 exports.getAllDevicesNew = async (req, res) => {
     try {
--- routes/testRoutes.js
+++ routes/testRoutes.js
@@ -1,8 +1,10 @@
-const { getAllUsersNew, getAllDevicesNew } = require('../controllers/testRouteController')
+const { getAllUsersNew, getUserByUserIdNew, getAllDevicesNew } = require('../controllers/testRouteController')
 
 const router = require('express').Router()
 
 router.get('/all-users', getAllUsersNew)
+
+router.get('/all-users/:id', getUserByUserIdNew)
 
 router.get('/all-devices', getAllDevicesNew)
 
