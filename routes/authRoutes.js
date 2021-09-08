f6d973b287cd47b7892039759023e25c9ed8abcd
Saman Arshad <saman.arshad97@gmail.com>
2021-09-08 09:30:28 +0530

    E: Removed ununsed code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,161 +1,25 @@
-const { body, check } = require('express-validator')
-
 const { signUp, adminSignup, adminLogin, loginUsingEmail, loginUsingMob, forgotPwd, resetPwd, logout } = require('../controllers/authController.js')
 const { isAuth, isUser, isAdmin } = require('../middlewares/authMiddleware')
 
 const router = require('express').Router()
 
-/**
- * @swagger
- * /api/v1/auth/signup:
- *   post:
- *     description: Signup user
- *     produces:
- *     - "application/json"  
- *     parameters:
- *     - in: "body"
- *       name: "body"
- *       description: "User details"
- *       required: true
- *       schema:    
- *        $ref: "#/definitions/Signupdata"
- *     responses:
- *       201:
- *         description: User signed up successfully.
- *       400:
- *         description: Invalid data.
- *       409:
- *         description: Duplicate data found.     
- */
-router.post('/signup',
-    check('email', "Invalid email address.").isEmail().normalizeEmail(),
-    body('firstName', 'First name is required.').not().isEmpty().trim().escape(),
-    check('firstName', 'First name length should not be > 50.').isLength({ max: 50 }),
-    body('lastName', 'Last name is required.').not().isEmpty().trim().escape(),
-    check('lastName', 'Last name length should not be > 50.').isLength({ max: 50 }),
-    body('gender', 'Gender is required.').not().isEmpty(),
-    body('phone', 'Phone number is required.').not().isEmpty(),
-    body('country', 'Gender is required.').not().isEmpty().escape(),
-    check('password', "Empty password.").not().isEmpty().trim().bail(),
-    check('password', "Password length should not be < 6").isLength({ min: 6 }),
-    signUp
-)
+router.post('/signup', signUp)
 
 router.post('/admin/signup', adminSignup)
 
 router.post('/admin/login', adminLogin)
 
-/**
- * @swagger
- * /api/v1/auth/loginUsingEmail:
- *   post:
- *     description: Login user
- *     produces:
- *     - "application/json"
- *     parameters:
- *     - in: "body"
- *       name: "body"
- *       description: "User login details"
- *       required: true
- *       schema:    
- *        $ref: "#/definitions/Loginusingemail"
- *     responses:
- *       200:
- *         description: User logged in successfully.
- *       400:
- *         description: Please enter your credentials.
- *       401:
- *         description: Incorrect credentials.   
- *       404:
- *         description: Account does not exist.  
- */
-router.post('/loginUsingEmail',
-    body('email').isEmail().normalizeEmail(),
-    body('password').not().isEmpty().trim(),
-    loginUsingEmail
-)
+router.post('/loginUsingEmail', loginUsingEmail)
 
-/**
- * @swagger
- * /api/v1/auth/loginUsingMob:
- *   post:
- *     description: Login user using mobile number
- *     produces:
- *     - "application/json"
- *     parameters:
- *     - in: "body"
- *       name: "body"
- *       description: "User login details"
- *       required: true
- *       schema:    
- *        $ref: "#/definitions/Loginusingmob"
- *     responses:
- *       200:
- *         description: User logged in successfully.
- *       400:
- *         description: Please enter your credentials.
- *       401:
- *         description: Incorrect credentials.   
- *       404:
- *         description: Account does not exist.  
- */
-router.post('/loginUsingMob',
-    body('phone').not().isEmpty().trim(),
-    body('password').not().isEmpty().trim(),
-    loginUsingMob
-)
+router.post('/loginUsingMob', loginUsingMob)
 
-/**
- * @swagger
- * /api/v1/auth/forgotPassword:
- *   post:
- *     description: Forgot password 
- *     produces:
- *     - "application/json"
- *     parameters:
- *     - in: "body"
- *       name: "body"
- *       description: "User email address"
- *       required: true
- *       schema:    
- *        $ref: "#/definitions/Forgotpassword"
- *     responses:
- *       404:
- *         description: No user found with this email address.
- *       400:
- *         description: Please enter email address. 
- *       200:
- *         description: Reset URL in response   
- */
-router.post('/forgotPassword',
-    check('email', "Invalid email coming from route validation").isEmail().normalizeEmail(),
-    forgotPwd
-)
+router.post('/forgotPassword', forgotPwd)
 
-router.patch('/resetPassword',
-    [
-        check('password', "Empty password").not().isEmpty().trim().bail(),
-        check('password', "Password length < 6").isLength({ min: 6 })
-    ],
-    resetPwd
-)
+router.patch('/resetPassword', resetPwd)
 
 // Protected Routes starts here
 router.use(isAuth)
 
-/**
- * @swagger
- * /api/v1/auth/logout:
- *   get:
- *     description: Logout user
- *     responses:
- *       200:
- *         description: You have logged out successfully.
- *       404:
- *         description: User not found.
- *     security:
- *     - bearerAuth: []      
- */
 router.get('/logout', logout)
 
 module.exports = router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,3 @@
-const { body } = require('express-validator')
-
 const { getAllUsers, getLoggedInUser, changePassword, updateProfile, deactivateAccount, deleteAccount } = require('../controllers/userController.js')
 const { isAuth, isUser, isAdmin } = require('../middlewares/authMiddleware')
 
@@ -8,124 +6,16 @@ const router = require('express').Router()
 // Protected Routes starts here
 router.use(isAuth)
 
-/**
- * @swagger
- * /api/v1/users/:
- *   get:
- *     description: Get all users
- *     responses:
- *       200:
- *         description: Array of users.      
- */
 router.get('/', isAdmin, getAllUsers)
 
-/**
- * @swagger
- * /api/v1/users/myProfile:
- *   get:
- *     description: Get data of logged in user
- *     responses:
- *       200:
- *         description: Data of logged in user.   
- *     security:
- *     - bearerAuth: []    
- */
 router.get('/myProfile', getLoggedInUser)
 
-/**
- * @swagger
- * /api/v1/users/changePassword:
- *   post:
- *     description: Change user password
- *     produces:
- *     - "application/json"
- *     parameters:
- *     - in: "body"
- *       name: "body"  
- *       description: "User old and new password"  
- *       required: true 
- *       schema:
- *         $ref: "#/definitions/Changepassword"
- *     responses:
- *       400:
- *         description: Invalid data.
- *       401:
- *         description: Your current password is incorrect.
- *       404:
- *         description: User not found.
- *       200:
- *         description: Password changed successfully.
- *     security:
- *     - bearerAuth: []      
- */
-router.post('/changePassword', body('passwordOld').not().isEmpty().trim(),
-    body('passwordUpdated').not().isEmpty().trim(),
-    changePassword
-)
+router.post('/changePassword', changePassword)
 
-/**
- * @swagger
- * /api/v1/users/updateProfile:
- *   put:
- *     description: Update user profile
- *     produces:
- *     - "application/json"
- *     parameters:
- *     - in: "body"
- *       name: "body"  
- *       description: "User profile details"  
- *       required: true 
- *       schema:
- *         $ref: "#/definitions/Updateprofile"
- *     responses:
- *       400:
- *         description: Invalid data.
- *       404:
- *         description: User not found.
- *       409:
- *         description: Duplicate data found.
- *       200:
- *         description: User details updated successfully
- *     security:
- *     - bearerAuth: []      
- */
-router.put('/updateProfile',
-    body('firstName').not().isEmpty().trim().escape(),
-    body('lastName').not().isEmpty().trim().escape(),
-    body('country').not().isEmpty().escape(),
-    body('gender').not().isEmpty(),
-    updateProfile
-)
+router.put('/updateProfile', updateProfile)
 
-
-/**
- * @swagger
- * /api/v1/users/deactivateAccount:
- *   post:
- *     description: Deactivate user account
- *     responses:
- *       204:
- *         description: Account Deleted.
- *       404:
- *         description: User not found.
- *     security:
- *     - bearerAuth: []      
- */
 router.post('/deactivateAccount', deactivateAccount)
 
-/**
- * @swagger
- * /api/v1/users/deleteAccount:
- *   delete:
- *     description: Delete user account
- *     responses:
- *       204:
- *         description: Account Deleted.
- *       404:
- *         description: User not found.
- *     security:
- *     - bearerAuth: []      
- */
 router.delete('/deleteAccount', deleteAccount)
 
 module.exports = router
